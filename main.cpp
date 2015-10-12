e8fb57282d539020254e3f95767816c9e47810c4
Michael Kayser <mkayser@stanford.edu>
2015-10-12 00:44:58 -0700

    refactoring

--- main.cpp
+++ main.cpp
@@ -42,16 +42,27 @@ namespace std {
 
 }
 
-typedef std::vector<std::vector<int> > Parents;
 typedef std::vector<std::vector<bool> > Rows;
+
+struct Node {
+public:
+  int id;
+  std::vector <int> parents;
+
+  Node(int _id, std::vector<int> _parents) : id(_id), parents(_parents) 
+  { }
+};
 
 class Graph {
 public:
   int n;
-  Parents parents;
+  std::vector<Node> nodes;
 
   Graph(int _n) : n(_n) {
-    parents = Parents(n);
+    std::vector<int> empty;
+    for(int i=0; i<n; i++) {
+      nodes.push_back(Node(i,empty));
+    }
   }
 };
 
@@ -128,24 +139,25 @@ public:
   Scorer(double alpha) : _alpha(alpha) {
   }
 
-  void compute_node_scores(Graph& graph, Rows& rows, std::vector<int>& nodes_to_update, std::vector<double>& node_scores) {
+  void compute_node_scores(std::vector<Node>& nodes, Rows& rows, std::vector<double>& node_scores) {
     node_scores.clear();
 
     std::vector<std::unordered_map<std::vector<int>, int > > observed_parent_configurations;
     std::vector<std::vector<std::vector<int> > > m; // node -> pconfindex -> nodeval -> count
     int n = rows[0].size();
 
-    observed_parent_configurations.resize(n);
-    m.resize(n);
+    observed_parent_configurations.resize(nodes.size());
+    m.resize(nodes.size());
 
     for(auto r: rows) {
-      for(auto i: nodes_to_update) {
-	int val = r[i] ? 1 : 0;
+      for(int i=0; i<nodes.size(); i++) {
+	Node& n = nodes[i];
+	int val = r[n.id] ? 1 : 0;
 
 	// make pconf
 	std::vector<int> pconf;
-	for(auto pnode: graph.parents[i]) {
-	  pconf.push_back(r[pnode]);
+	for(auto pid: n.parents) {
+	  pconf.push_back(r[pid]);
 	}
 
 	// get index of pconf (make index if necessary)
@@ -161,7 +173,8 @@ public:
 
     double a_ij0 = _alpha * 2;
 
-    for(auto i: nodes_to_update) {
+    for(int i=0; i<nodes.size(); i++) {
+      Node& n = nodes[i];
       double node_score = 0.0;
       for(int j=0; j<m[i].size(); j++) {
 	int m_ij0 = std::accumulate(m[i][j].begin(), m[i][j].end(), 0);
@@ -176,12 +189,7 @@ public:
     }
   }
 
-  void compute_all_node_scores(Graph& graph, Rows& rows, std::vector<double>& node_scores) {
-    std::vector<int> nodes_to_update(graph.n);
-    std::generate(nodes_to_update.begin(), nodes_to_update.end(), RangeGen());
-    compute_node_scores(graph, rows, nodes_to_update, node_scores);
-  }
-  
+
 private:
   double _alpha;
 };
@@ -192,7 +200,17 @@ public:
   Type type;
   int parent;
   int child;
+
+  Action(Type t, int p, int c) : type(t), parent(p), child(c) {
+  }
 };
+
+class NodeQueue {
+public:
+  std::priority_queue<ScoredNode> queue;
+  
+};
+
 
 struct SearchState {
 public:
@@ -201,9 +219,45 @@ public:
     std::vector<double> score_deltas;
     std::vector<bool> valid;
     std::vector<bool> dirty;
-    void init(Graph) {
+
+    std::size_t size() { return actions.size(); }
+    // Does not initialize score_deltas, confusingly
+    void init(Graph& g) {
+      for(int child=0; child<g.n; child++) {
+	for(int parent=0; parent<g.n; parent++) {
+	  for(int type=0; type<=Action::REVERSE; type++) {
+	    Action action(type,parent,child);
+	    bool _valid;
+	    bool _dirty = false;
+
+	    if(type == Action::ADD) {
+	      // valid if the edge does not exist
+	      std::vector<int>& p = g.nodes[child].parents;
+	      _valid = (std::find(p.begin(), p.end(), parent)==p.end());
+	    }
+	    else if(type == Action::REMOVE) {
+	      // valid if the edge does exist
+	      std::vector<int>& p = g.nodes[child].parents;
+	      _valid = (std::find(p.begin(), p.end(), parent)!=p.end());
+	    }
+	    else {
+	      std::vector<int>& p = g.nodes[child].parents;
+	      _valid = (std::find(p.begin(), p.end(), parent)!=p.end());
+	    }
+	    
+	    // At this point all scores are uncomputed and need updates
+	    _dirty = _valid;
+
+	    actions.push_back(action);
+	    valid.push_back(_valid);
+	    dirty.push_back(_dirty);
+	    score_deltas.push_back(0);
+	  }
+	}
+      }
     }
   };
+
   struct Score {
     std::vector<double> node_scores;
     std::vector<bool> dirty;
@@ -218,14 +272,23 @@ public:
   ActionSet actions;
 
   SearchState(Graph& g, Scorer& s, Rows& r) : graph(g), scorer(s), rows(r) {
-    scorer.compute_all_node_scores(graph, rows, score.node_scores);
+    scorer.compute_node_scores(graph.nodes, rows, score.node_scores);
     score.total_score = std::accumulate(score.node_scores.begin(), score.node_scores.end(), 0);
-    actions.init(g.n);
+    actions.init(g);
+    update_score_deltas();
   }
 
-  void update(Graph& graph, Scorer& scorer, Rows& rows, Action action) {
-    // TODO updates the graph, action set and scores
+  void 
+
+  void update_score_deltas() {
+    for(int i=0; i<actions.size(); i++) {
+      if(actions.dirty
+    }
+    // TODO: for each dirty element, add to node vector
+    // score all decisions
+    // compute delta by subtracting out previous node scores and adding new node scores
   }
+
 };
 
 class LocalSearch {
